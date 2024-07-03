d34b641349926f48b68ca31c533c6ba82e149d26
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-03 17:31:41 +0200

    safafd

--- smi.sh
+++ smi.sh
@@ -21,36 +21,36 @@ read_gpu_model() {
 }
 
 
+kill_background_jobs() {
+    for pid in $@; do
+        kill $pid
+    done
+}
+
+
+# Function to log GPU usage for each GPU on the node
+log_gpu_usage() {
+  local gpu_ids=(${CUDA_VISIBLE_DEVICES//,/ })
+  for gpu_id in "${gpu_ids[@]}"; do
+    nvidia-smi -i ${gpu_id} -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits >> logs/gpu_usage_node${SLURM_NODEID}_gpu${gpu_id}.log &
+  done
+}
+
+# Main script
+
 read_gpu_model
 
-# kill_background_jobs() {
-#     for pid in $@; do
-#         kill $pid
-#     done
-# }
+gpu_ids=(${CUDA_VISIBLE_DEVICES//,/ })
+for gpu_id in "${gpu_ids[@]}"; do
+nvidia-smi -i ${gpu_id} -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits >> logs/gpu_usage_node${SLURM_NODEID}_gpu${gpu_id}.log &
+done
 
 
-# # Function to log GPU usage for each GPU on the node
-# log_gpu_usage() {
-#   local gpu_ids=(${CUDA_VISIBLE_DEVICES//,/ })
-#   for gpu_id in "${gpu_ids[@]}"; do
-#     nvidia-smi -i ${gpu_id} -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits >> logs/gpu_usage_node${SLURM_NODEID}_gpu${gpu_id}.log &
-#   done
-# }
+# srun log_gpu_usage &  # Run the logging function in the background
 
-# # Main script
+# Run the benchmark
+srun python3 resnet_multi.py >> logs/training_output_${SLURM_JOB_ID}.log
 
-# gpu_ids=(${CUDA_VISIBLE_DEVICES//,/ })
-# for gpu_id in "${gpu_ids[@]}"; do
-# nvidia-smi -i ${gpu_id} -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits >> logs/gpu_usage_node${SLURM_NODEID}_gpu${gpu_id}.log &
-# done
-
-
-# # srun log_gpu_usage &  # Run the logging function in the background
-
-# # Run the benchmark
-# srun python3 resnet_multi.py >> logs/training_output_${SLURM_JOB_ID}.log
-
-# #kill of background logging
-# bg_pids=$(jobs -p)
-# kill_background_jobs $bg_pids
+#kill of background logging
+bg_pids=$(jobs -p)
+kill_background_jobs $bg_pids
