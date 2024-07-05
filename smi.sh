9cc690cbe092c1ae87a3e3c7bacfdf0d723d0bfc
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-05 16:46:04 +0200

    asdfsadfs

--- smi.sh
+++ smi.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=brook
 #SBATCH --job-name=bert_multi
 #SBATCH --output=rolling_output_nojobnumber.out
-#SBATCH --nodes=2 
+#SBATCH --nodes=1 
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 
 
 
