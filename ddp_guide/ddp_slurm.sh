2ec1ccbbdaf30fc6791c961bbc7ea59dca95b297
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-11 17:12:37 +0200

    dfasfa

--- ddp_guide/ddp_slurm.sh
+++ ddp_guide/ddp_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=multinode-example
-#SBATCH --nodes=4
-#SBATCH --ntasks=4
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=4
 
@@ -15,7 +15,7 @@ echo Node IP: $head_node_ip
 export LOGLEVEL=INFO
 
 srun torchrun \
---nnodes 4 \
+--nnodes 2 \
 --nproc_per_node 1 \
 --rdzv_id $RANDOM \
 --rdzv_backend c10d \
