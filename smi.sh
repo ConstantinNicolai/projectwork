166edfb4261af1033fe9ff805cac9c8fd1096829
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-11 14:23:03 +0200

    sadfasdf

--- smi.sh
+++ smi.sh
@@ -49,7 +49,7 @@ done
 # srun log_gpu_usage &  # Run the logging function in the background
 
 # Run the benchmark
-srun python3 resnet_multi.py >> logs/training_output_${SLURM_JOB_ID}.log
+srun python3 distributed_data_parallel.py >> logs/training_output_${SLURM_JOB_ID}.log
 
 #kill of background logging
 bg_pids=$(jobs -p)
