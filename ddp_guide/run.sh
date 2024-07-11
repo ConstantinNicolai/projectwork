7c1266edddc21abbd60ef8c71ef75cec3cf230ec
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-11 17:32:47 +0200

    asfsafsd

--- ddp_guide/run.sh
+++ ddp_guide/run.sh
@@ -4,11 +4,11 @@
 #SBATCH --output=ddp_%j.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 
 # Check if Nvidia SMI is installed
 
 srun torchrun \
 --standalone \
---nproc_per_node=4 \
+--nproc_per_node=2 \
 multgpu_singlenode_torchrun.py 50 10
