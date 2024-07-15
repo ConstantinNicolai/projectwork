d708642deb0bb127c1e7c9729466fd3ca9943ae1
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-15 15:16:46 +0200

    asdfsff

--- torchrun.sh
+++ torchrun.sh
@@ -17,7 +17,7 @@ ip a
 MASTER_HOSTNAME=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 
 # Get the IPv4 address of the first node
-MASTER_ADDR=$(ip a show enp** | grep -oP 'inet \K[\d.]+'| head -n 1)
+MASTER_ADDR=$(ip a | grep -oE 'enp[0-9]+' | grep -oP 'inet \K[\d.]+'| head -n 1)
 # MASTER_ADDR=$(ssh $MASTER_HOSTNAME hostname -I | awk '{print $1}')
 
 # Export the MASTER_ADDR and set MASTER_PORT
