98dd1007b4c22b0ed2dae3734083c88e1be39af1
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-15 15:12:55 +0200

    sfdasfs

--- torchrun.sh
+++ torchrun.sh
@@ -16,7 +16,7 @@ conda activate constabass
 MASTER_HOSTNAME=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
 
 # Get the IPv4 address of the first node
-MASTER_ADDR=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n 1)
+MASTER_ADDR=$(ip a show eno1np0 | grep -oP 'inet \K[\d.]+')
 # MASTER_ADDR=$(ssh $MASTER_HOSTNAME hostname -I | awk '{print $1}')
 
 # Export the MASTER_ADDR and set MASTER_PORT
