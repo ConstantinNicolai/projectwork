579446de3b38bde727cab4904080aa096688a241
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-15 15:27:11 +0200

    sadfsaasdfasdf

--- torchrun.sh
+++ torchrun.sh
@@ -11,22 +11,5 @@
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate constabass
 
-# # Get the hostname of the first node
-# MASTER_HOSTNAME=$(scontrol show hostname $SLURM_NODELIST | head -n 1)
-
-# # Get the IPv4 address of the first node
-# MASTER_ADDR=$(ip a | grep -oE 'enp[0-9]+' | grep -oP 'inet \K[\d.]+'| head -n 1)
-# # MASTER_ADDR=$(ssh $MASTER_HOSTNAME hostname -I | awk '{print $1}')
-
-# # Export the MASTER_ADDR and set MASTER_PORT
-# export MASTER_ADDR
-# export MASTER_PORT=29400
-
-# # # Print SLURM environment variables (for debugging purposes)
-# # printenv | grep SLURM
-
-# # # torchrun --nnodes=2 --nproc_per_node=1 torchrun_test.py
-
-# # echo $MASTER_ADDR
 
 torchrun --nnodes=2 --nproc-per-node=1 --max-restarts=2 --rdzv-id=5634 --rdzv-backend=c10d --rdzv-endpoint=147.142.43.167  torchrun_test.py
