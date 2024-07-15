6ebd4ec891476a1631bbb178c5bdbfea5dacfd21
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-15 13:58:26 +0200

    sdfasfsdaf

--- torchrun.sh
+++ torchrun.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=brook
 #SBATCH --output=torchrun.out
 #SBATCH --nodes=1                # node count
-#SBATCH --ntasks-per-node=2      # total number of tasks per node
-#SBATCH --gres=gpu:2             # number of allocated gpus per node
+#SBATCH --ntasks-per-node=4      # total number of tasks per node
+#SBATCH --gres=gpu:4             # number of allocated gpus per node
 
 
 # load appropriate conda paths, because we are not in a login shell
@@ -12,4 +12,4 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate constabass
 
 
-torchrun --nproc_per_node=2 --nnodes=1 torchrun_test.py
+torchrun --nproc_per_node=4 --nnodes=1 torchrun_test.py
