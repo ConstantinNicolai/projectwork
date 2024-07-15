f598a331aa5b558682102bb8f9a9e541286ee030
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-15 14:53:36 +0200

    asdfsda

--- torchrun.sh
+++ torchrun.sh
@@ -12,4 +12,6 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate constabass
 
 
-torchrun --nnodes=2 --nproc_per_node=1 torchrun_test.py
+# torchrun --nnodes=2 --nproc_per_node=1 torchrun_test.py
+
+torchrun --nnodes=2 --nproc-per-node=1 --max-restarts=2 --rdzv-id=5634 --rdzv-backend=c10d --rdzv-endpoint=HOST_NODE_ADDR
