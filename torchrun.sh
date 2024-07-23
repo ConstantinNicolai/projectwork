29d0f65e466e05f33295cf5b33f3e3ac7fd10783
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-07-23 11:17:32 +0200

    sadfasf

--- torchrun.sh
+++ torchrun.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=torchrun     # create a short name for your job
 #SBATCH --partition=brook
 #SBATCH --output=torchrun.out
-#SBATCH --nodes=2                # node count
+#SBATCH --nodes=1                # node count
 #SBATCH --ntasks-per-node=4      # total number of tasks per node
 #SBATCH --gres=gpu:4             # number of allocated gpus per node
 
@@ -12,4 +12,4 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda activate constabass
 
 
-torchrun --nnodes=2 --nproc-per-node=4  torchrun_test.py --max-restarts=2 --rdzv-id=5634 --rdzv-backend=c10d --rdzv-endpoint=147.142.43.167
+torchrun --nnodes=1 --nproc-per-node=4  torchrun_test.py --max-restarts=2 --rdzv-id=5634 --rdzv-backend=c10d --rdzv-endpoint=147.142.43.167
