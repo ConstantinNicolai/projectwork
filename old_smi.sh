968981b902fd84a0cc86d0fc1d3c85fa31e38417
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-02 17:22:32 +0200

    removed the check for correct input since it does not help in this non interactiv eshell

--- old_smi.sh
+++ old_smi.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 MODEL_NAME BATCH_SIZE GPU_MODEL NUM_GPUS"
-  exit 1
-fi
-
 # Variables for the model name, batch size, GPU model, and number of GPUs
 MODEL_NAME=$1
 BATCH_SIZE=$2
