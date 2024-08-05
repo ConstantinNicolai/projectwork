3627fff45f6213349aa2aee6b61dd8bd3a956523
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-08-05 13:47:48 +0200

    titnaxp single gpu

--- autosbatch/submit_jobs.sh
+++ autosbatch/submit_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the lists of GPU counts and models
-gpu_counts=(1 2 3)
-gpu_models=("TitanXp" "K80")
+gpu_counts=(1)
+gpu_models=("TitanXp")
 
 # Create a log file
 log_file="submission_log.txt"
